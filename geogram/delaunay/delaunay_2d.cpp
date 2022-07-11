2e6f1a78259460ee0389d62cfabafc7d4ec8676f
Simone Gasparini <simone.gasparini@gmail.com>
2022-07-11 12:15:42 +0200

    [geogram-delaunay] Update to 1.7.9

--- geogram/delaunay/delaunay_2d.cpp
+++ geogram/delaunay/delaunay_2d.cpp
@@ -249,7 +249,12 @@ namespace GEO {
             // Do not re-insert the first four vertices.
             if(v != v0 && v != v1 && v != v2) {
                 index_t new_hint = insert(v, hint);
-                if(new_hint != NO_TRIANGLE) {
+		if(new_hint == NO_TRIANGLE) {
+		  has_empty_cells_ = true;
+		  if(abort_if_empty_cell_) {
+		    return;
+		  }
+		} else {
                     hint = new_hint;
                 }
             }
@@ -385,6 +390,11 @@ namespace GEO {
             nb_triangles,
             cell_to_v_store_.data(), cell_to_cell_store_.data()
         );
+
+	// Not mandatory, but doing so makes it possible to
+	// use locate() in derived classes outside of
+	// set_vertices().
+	cell_next_.assign(cell_next_.size(),~index_t(0));
     }
 
     index_t Delaunay2d::nearest_vertex(const double* p) const {
